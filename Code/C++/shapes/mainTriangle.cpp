bfc59d1a11f74194d7510b58116e56cddf07742e
Patrick Hughes <pjhughes@ucsd.edu>
2019-07-23 15:11:45 -0700

    pjh - C++ shape dump #2

--- Code/C++/shapes/mainTriangle.cpp
+++ Code/C++/shapes/mainTriangle.cpp
@@ -1,8 +1,8 @@
 #include "Triangle.h"
 
 int main(int argc, char **argv) {
-  Triangle s1(12.0,5.0);
-  s1.PrintArea(std::cout);
+  Triangle theTriangle(12.0,5.0);
+  theTriangle.PrintArea(std::cout);
   return 0;
 }
 
