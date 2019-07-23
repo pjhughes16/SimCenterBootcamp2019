dc95e329aa07590eb67b4e092c35e7ae857e41c1
Patrick Hughes <pjhughes@ucsd.edu>
2019-07-23 15:21:39 -0700

    pjh - fixed mainTriangle.cpp

--- Code/C++/shapes/mainTriangle.cpp
+++ Code/C++/shapes/mainTriangle.cpp
@@ -4,16 +4,4 @@ int main(int argc, char **argv) {
   Triangle theTriangle(12.0,5.0);
   theTriangle.PrintArea(std::cout);
   return 0;
-}
-
-
-
-#include "Triangle.h"
-
-int main(int argc, char **argv) {
-  Triangle s1(12.0,5.0);
-  s1.PrintArea(std::cout);
-  return 0;
-}
-
-
+};
